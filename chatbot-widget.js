cb139693cd0d2fb388df7269a9d3a64ee36f276c
Marceleanos27 <lehocky.business@gmail.com>
2025-08-22 17:47:35 +0200

    Update chatbot-widget.js

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -3,7 +3,7 @@
   window.__marcelChatbotLoaded = true;
 
   const iframe = document.createElement("iframe");
-  iframe.src = "https://grandenassistantai-zazj.vercel.app/"; // ⬅️ Change to your real URL (e.g. Vercel)
+  iframe.src = "https://grandenassistantai-sigma.vercel.app/"; // ⬅️ Change to your real URL (e.g. Vercel)
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
@@ -15,3 +15,4 @@
   iframe.style.boxShadow = "0 10px 25px rgba(0,0,0,0.0)";
   document.body.appendChild(iframe);
 })();
+
